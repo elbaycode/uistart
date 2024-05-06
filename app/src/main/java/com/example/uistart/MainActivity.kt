7037035fe58469c5089fcda4ef0309009581aa44
elbaycode <alisoy.elbayi.khudaddin.2021@unec.edu.az>
2024-05-06 14:49:50 +0400

    full

---
+++ app/src/main/java/com/example/uistart/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.uistart
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import coil.load
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.squareup.picasso.Picasso
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+    }
+}
